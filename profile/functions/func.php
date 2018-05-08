45ded781dda968d33f67b6f9bd39266f41cb26c7
Primoz <primoz969@gmail.com>
2018-05-08 22:01:45 +0200

    styles left side of profile page

---
+++ profile/functions/func.php
@@ -0,0 +1,46 @@
+<?php include '../connection/db.php'; ?>
+<?php 
+	function links() {
+		GLOBAL $db;
+		$user_id = $_SESSION['user_id'];
+		$query = $db->prepare("SELECT * FROM users WHERE id = ?");
+		$query->execute(array($user_id));
+		$r = $query->fetch(PDO::FETCH_OBJ);
+
+		if(empty($r->image)) {
+			$photo = "<img src='img/unknown.png' class='user_img'>";
+			$photo_link = "<a href=''>Update photo <i class='fa fa-pencil'></i></a>";
+		} else {
+			$photo = "<img src='img/$r->image' class='user_img'>";
+			$photo_link = "<a href=''>Update photo <i class='fa fa-pencil'></i></a>";
+		}
+
+		if(empty($r->bio)) {
+			$bio = "<a href=''>Add Bio <i class='fa fa-plus-circle'></i>";
+		} else {
+			$bio = "<a href=''>Update Bio <i class='fa fa-pencil'></i></a>";
+		}
+
+		if(empty($r->facebook)) {
+			$facebook = "<a href=''>Add Facebook <i class='fa fa-plus-circle'></i>";
+		} else {
+			$facebook = "<a href=''>Update Facebook <i class='fa fa-pencil'></i></a>";
+		}
+
+		if(empty($r->linkedin)) {
+			$linkedin = "<a href=''>Add Linkedin <i class='fa fa-plus-circle'></i></a>";
+		} else {
+			$linkedin = "<a href=''>Update LinkedIn <i class='fa fa-pencil'></i></a>";
+		}
+
+		echo "<ul class='list-group'>
+			$photo
+			<li class='list-group-item update-photoLi'>$photo_link</li>
+			<li class='list-group-item'>$bio</li>
+			<li class='list-group-item'>$facebook</li>
+			<li class='list-group-item'>$linkedin</li>
+			<li class='list-group-item'><a href=''>Update Password <i class='fa fa-pencil'></i></a></li>
+			<li class='list-group-item'><a href=''>Update Name <i class='fa fa-pencil'></i></a></li>
+		</ul>";
+	}
+?>
--- profile/index.php
+++ profile/index.php
@@ -1,1 +1,42 @@
-<h1>Login</h1>
+<?php include 'functions/func.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>User Profile</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="../assets/css/profile.css">
+</head>
+<body>
+	<?php include '../parts/nav.php'; ?>
+	<div class="container contents">
+		<div class="row">
+			<div class="col-md-3">
+				<div class="left-area">
+					<?php links(); ?>
+				</div> <!-- End left-area -->
+			</div> <!-- End col-3 -->
+			<div class="col-md-9">
+				<div class="right-area">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</div> <!-- End right-area -->
+			</div> <!-- End col-9 -->		
+		</div> <!-- End row -->
+	</div> <!-- End Container -->
+
+	<script
+  	src="https://code.jquery.com/jquery-3.3.1.js"
+  	integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+  	crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	
+</body>
+</html>
